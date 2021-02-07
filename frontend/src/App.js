5a9ef9802b364fdf67c6457627cae7bf645c9610
lovro <hodak.lovro@gmail.com>
2021-02-07 23:05:11 +0100

    maybe now we will get data from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,9 @@ function App() {
       console.log(response.data)
       setItems(response.data)
     })
+    .catch((err) => {
+      console.log('this is error: ', err)
+    })
   }, [])
 
   return (
